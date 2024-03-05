ba0f6bb4d5f6364b039b82cdcdf310f2e4132141
Lea <leagraas@hotmail.com>
2024-03-05 10:54:57 +0100

    Ok jusque Build with Webpack

---
+++ init/front-end/src/main.js
@@ -0,0 +1,28 @@
+// Problème de import export
+
+import { Router } from "./app/scripts/router";
+// TODO #import-css: use ES side-effect imports to import styles/style.css
+
+const outlet = document.querySelector("#content-outlet");
+const router = new Router(outlet);
+router
+  .register("", {
+    component: WelcomeComponent,
+    // TODO #import-html: remove the templateUrl property.
+    templateUrl: "/src/app/views/welcome.html",
+  })
+  .register("welcome", {
+    component: WelcomeComponent,
+    // TODO #import-html: remove the templateUrl property.
+    templateUrl: "/src/app/views/welcome.html",
+  })
+  .register("game", {
+    component: GameComponent,
+    // TODO #import-html: remove the templateUrl property.
+    templateUrl: "/src/app/views/game.html",
+  })
+  .register("score", {
+    component: ScoreComponent,
+    // TODO #import-html: remove the templateUrl property.
+    templateUrl: "/src/app/views/score.html",
+  });
