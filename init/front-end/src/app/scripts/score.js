fd4328770e88d8d39de84d98fd3b41613105e5b0
Lea <leagraas@hotmail.com>
2024-03-07 16:11:47 +0100

    Classes ok

--- init/front-end/src/app/scripts/score.js
+++ init/front-end/src/app/scripts/score.js
@@ -1,24 +1,26 @@
-// TODO #class: use the ES6 class keyword
 import { parseUrl } from "./utils";
 import template from "../views/score.html";
+import { Component } from "./component";
+
 /* class ScoreComponent constructor */
-export function ScoreComponent() {
-  // TODO #extends: call super(template)
-  var params = parseUrl();
-  this.name = params.name;
-  this.size = parseInt(params.size);
-  this.time = parseInt(params.time);
-  this.template = template;
+export class ScoreComponent extends Component {
+  constructor() {
+    super(template);
+    var params = parseUrl();
+    this.name = params.name;
+    this.size = parseInt(params.size);
+    this.time = parseInt(params.time);
+    this.template = template;
+  }
+
+  // put component in global scope, to be runnable right from the HTML.
+  // window.ScoreComponent = ScoreComponent;
+
+  /* method ScoreComponent.init */
+  init() {
+    document.getElementById("name").innerText = this.name;
+    document.getElementById("size").innerText = this.size;
+    document.getElementById("time").innerText = this.time;
+  };
 }
-
-// put component in global scope, to be runnable right from the HTML.
-// window.ScoreComponent = ScoreComponent;
-
-// TODO #class: turn function into a method of ScoreComponent
-/* method ScoreComponent.init */
-ScoreComponent.prototype.init = function init() {
-  document.getElementById("name").innerText = this.name;
-  document.getElementById("size").innerText = this.size;
-  document.getElementById("time").innerText = this.time;
-};
 
--- init/front-end/src/app/scripts/welcome.js
+++ init/front-end/src/app/scripts/welcome.js
@@ -1,43 +1,45 @@
 import template from "../views/welcome.html";
-// TODO #class: use the ES6 class keyword
+import { Component } from "./component";
+
 /* class WelcomeComponent constructor  */
-export function WelcomeComponent() {
-  // TODO #extends: call super(template)
-    this.template = template;
-}
+export class WelcomeComponent extends Component {
+    constructor() {
+        super(template);
+        this.template = template;
+    }
 
 // put component in global scope, to be runnable right from the HTML.
 // window.WelcomeComponent = WelcomeComponent;
 
-// TODO #class: turn function into a method of WelcomeComponent
-/* method WelcomeComponent.init */
-WelcomeComponent.prototype.init = function init() {
-  var form = document.querySelector("form.form-signin");
+    /* method WelcomeComponent.init */
+    init() {
+        var form = document.querySelector("form.form-signin");
 
-  form.addEventListener(
-    "submit",
-    // TODO #arrow-function: use arrow function instead.
-    function (event) {
-      event.preventDefault();
-      if (form.checkValidity() === false) {
-        event.stopPropagation();
-        form.classList.add("was-validated");
-      } else {
-        var name = event.srcElement.querySelector("#nickname").value;
-        var size = parseInt(event.srcElement.querySelector("#size").value);
+        form.addEventListener(
+            "submit",
+            // TODO #arrow-function: use arrow function instead.
+            function (event) {
+                event.preventDefault();
+                if (form.checkValidity() === false) {
+                    event.stopPropagation();
+                    form.classList.add("was-validated");
+                } else {
+                    var name = event.srcElement.querySelector("#nickname").value;
+                    var size = parseInt(event.srcElement.querySelector("#size").value);
 
-        _startGame(name, size);
-      }
-    }.bind(this),
-    false
-  );
+                    this._startGame(name, size); //scope de classe et fonction et le mot dys
+                }
+            }.bind(this),
+            false
+        );
 
-  return this;
-};
+        return this;
+    };
 
-// TODO #class: turn function into a method of WelcomeComponent
-function _startGame(name, size) {
-  var gamePage = "./#game";
-  // TODO #template-literals:  use template literals (backquotes)
-  window.location = gamePage + "?name=" + name + "&size=" + size;
+    _startGame(name, size) {
+        var gamePage = "./#game";
+        // TODO #template-literals:  use template literals (backquotes)
+        window.location = gamePage + "?name=" + name + "&size=" + size;
+    }
 }
+
